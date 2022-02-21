190fd974d52b05f8224fa5c8a38f7f16e3962de7
Damian Studnicki <damian.studnicki@microsoft.wsei.edu.pl>
2022-02-21 18:27:52 +0100

    Adding automatic database update

--- WRPanel/WRPanel/Startup.cs
+++ WRPanel/WRPanel/Startup.cs
@@ -46,6 +46,12 @@ namespace WRPanel
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
